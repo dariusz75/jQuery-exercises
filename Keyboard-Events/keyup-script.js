2ea6a6f90345c84b97e26be34c5ab875cfb125cb
Dariusz Franczak <darius32@op.pl>
2016-06-29 09:53:59 +0100

    final solution

--- Keyboard-Events/keyup-script.js
+++ Keyboard-Events/keyup-script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   var price = +$(this).closest('.vacation').data('price');
   var quantity = +$(this).closest('.quantity').val();
   var totalPrice = (price * quantity).toFixed(2);
-  $('.total').text(totalPrice);
+  $(this).closest('.wrapper').find('.total').text(totalPrice);
    
    });
 });
