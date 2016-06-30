47f3f09bcd64663121752cd15762319adb1b92b3
Dariusz Franczak <darius32@op.pl>
2016-06-30 19:57:50 +0100

    AJAX-refactored

---
+++ AJAX-01-refactored/script.js
@@ -0,0 +1,38 @@
+var functionalities = {
+  inint: function() {
+
+  function showPhotos() {
+    $(this).find('span').slideToggle();
+  }
+  
+  $('.photos').on('mouseenter', 'li', showPhotos)
+              .on('mouseleave', 'li', showPhotos);
+
+  var el = $("#tour");
+  el.on("click", "button", function() {
+    $.ajax({
+      url: 'photos.html',
+      data: {location: el.data('location')},
+      success: function(response) {
+        $('.photos').html(response).fadeIn();
+      },
+      error: function() {
+        $('.photos').html('<li>There was a problem fetching the latest photos. Please try again.</li>');
+      },
+      timeout: 3000,
+      beforeSend: function() {
+        $('#tour').addClass('is-fetching');
+      },
+      complete: setTimeout(function(){
+        $('#message').html('<p>AJAX request succeeded!</p>')}, 1000)
+
+    });
+  });
+
+}
+}
+
+
+$(document).ready(function() {
+  functionalities.inint();
+});
