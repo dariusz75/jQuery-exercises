8615317bb806fef157725855b62363aedf4d046a
Dariusz Franczak <darius32@op.pl>
2016-07-04 11:22:07 +0100

    corrections added

--- AJAX-01-refactored/script.js
+++ AJAX-01-refactored/script.js
@@ -1,38 +1,40 @@
 var functionalities = {
+  showPhotos: function () {
+    $(this).find('span').slideToggle();
+  },
+  showDescription: function () {
+    $('.photos').on('mouseenter', 'li', this.showPhotos)
+                .on('mouseleave', 'li', this.showPhotos);
+  },
   inint: function() {
 
-  function showPhotos() {
-    $(this).find('span').slideToggle();
-  }
-  
-  $('.photos').on('mouseenter', 'li', showPhotos)
-              .on('mouseleave', 'li', showPhotos);
+    var el = $("#tour");
+    el.on("click", "button", function() {
+      $.ajax({
+        url: 'photos.html',
+        data: {location: el.data('location')},
+        success: function(response) {
+          $('.photos').html(response).fadeIn();
+        },
+        error: function() {
+          $('.photos').html('<li>There was a problem fetching the latest photos. Please try again.</li>');
+        },
+        timeout: 3000,
+        beforeSend: function() {
+          $('#tour').addClass('is-fetching');
+        },
+        complete: setTimeout(function(){
+          $('#message').html('<p>AJAX request succeeded!</p>')}, 1000)
 
-  var el = $("#tour");
-  el.on("click", "button", function() {
-    $.ajax({
-      url: 'photos.html',
-      data: {location: el.data('location')},
-      success: function(response) {
-        $('.photos').html(response).fadeIn();
-      },
-      error: function() {
-        $('.photos').html('<li>There was a problem fetching the latest photos. Please try again.</li>');
-      },
-      timeout: 3000,
-      beforeSend: function() {
-        $('#tour').addClass('is-fetching');
-      },
-      complete: setTimeout(function(){
-        $('#message').html('<p>AJAX request succeeded!</p>')}, 1000)
-
+      });
     });
-  });
-
-}
-}
+  }
+};
 
 
 $(document).ready(function() {
+
   functionalities.inint();
+  functionalities.showPhotos();
+  functionalities.showDescription();
 });
